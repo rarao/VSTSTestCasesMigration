cdae794063278c634cf0de1b07a5d5a135e50897
rarao <rahulrao.hsr@gmail.com>
2019-09-11 18:55:44 +0530

    Adding Test cases migration project for VSTS and managing test plans for VSTS

---
+++ VSTSTestCasesMigration/Program.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace VSTSTestCasesMigration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string uri = "https://dev.azure.com/VarinderKumar";
+            var project = VSTSOperations.GetTeamProject(uri, "RahulTCMigration");
+
+            //VSTSOperations.ManageTestPlans(uri, project, "rahultpNew");
+
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"\\10.26.1.19\Common-Data\Hydra\RahulRao\Foundation-Test Case-20190905\Foundation-Test Case-20190904.xls");
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[2];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            int rowCount = xlRange.Rows.Count;
+            int colCount = xlRange.Columns.Count;
+
+            Stack<string> areaPathStack = new Stack<string>();
+            areaPathStack.Push("RahulTCMigration\\Foundation");
+           
+            string AttachmentBaseDirectory = @"\\10.26.1.19\Common-Data\Hydra\RahulRao\Foundation-Test Case-20190905\";
+
+            for (int i = 2; i <= rowCount; i++)
+            {                
+                string suiteName = string.Empty;
+                int depth = 0;
+                if (xlRange.Cells[i, 1] != null && xlRange.Cells[i, 1].Value2 != null)
+                {
+                    if (xlRange.Cells[i, 2] == null || xlRange.Cells[i, 2].Value2 == null)
+                    {
+                        throw new Exception("excel is not formatted correctly.");
+                    }
+                    suiteName = xlRange.Cells[i, 2].Value2.ToString();
+                    suiteName = suiteName.Substring(suiteName.IndexOf(' ') + 1).Trim();
+
+                    depth = Convert.ToInt32(xlRange.Cells[i, 1].Value2.ToString());
+
+                    if (depth == -1)
+                        break;
+
+                    while (depth <= areaPathStack.Count - 1)
+                    {
+                        areaPathStack.Pop();
+                    }
+
+                    areaPathStack.Push(areaPathStack.Peek() + "\\" + suiteName);
+                }
+
+                Tuple<string, string>[] steps = new Tuple<string, string>[1];
+                string title = string.Empty;
+                string description = string.Empty;
+                string precondition = string.Empty;
+
+                string Product = string.Empty;
+                string Component = string.Empty;
+                string AutomationClassName = string.Empty;
+                string AutomationID = string.Empty;
+                string ReasonforNotAutomating = string.Empty;
+                string DeploymentMode = string.Empty;
+                string PAMTags = string.Empty;
+                string Applicable = string.Empty;
+
+                List<Tuple<string, string>> extraFields = new List<Tuple<string, string>>();
+                List<string> attachments = new List<string>();
+
+                if (xlRange.Cells[i, 3] != null && xlRange.Cells[i, 3].Value2 != null)
+                {
+                    if (xlRange.Cells[i, 9] != null && xlRange.Cells[i, 9].Value2 != null)
+                    {
+                        precondition = "preconditions \r\n" + xlRange.Cells[i, 9].Value2.ToString() + "\r\n\r\n\r\n";
+                    }
+
+                    if (xlRange.Cells[i, 11] != null && xlRange.Cells[i, 11].Value2 != null && xlRange.Cells[i, 12] != null && xlRange.Cells[i, 12].Value2 != null)
+                    {
+                        steps[0] = new Tuple<string, string>(precondition + xlRange.Cells[i, 11].Value2.ToString(), xlRange.Cells[i, 12].Value2.ToString());
+                    }
+                    else if (xlRange.Cells[i, 11] == null || xlRange.Cells[i, 11].Value2 == null)
+                    {
+                        steps[0] = new Tuple<string, string>(precondition, xlRange.Cells[i, 12].Value2.ToString());
+                    }
+                    else if (xlRange.Cells[i, 12] == null || xlRange.Cells[i, 12].Value2 == null)
+                    {
+                        steps[0] = new Tuple<string, string>(precondition + xlRange.Cells[i, 11].Value2.ToString(), string.Empty);
+                    }
+
+                    if (xlRange.Cells[i, 4] != null && xlRange.Cells[i, 4].Value2 != null)
+                    {
+                        title = xlRange.Cells[i, 4].Value2.ToString();
+                    }
+                    if (xlRange.Cells[i, 8] != null && xlRange.Cells[i, 8].Value2 != null)
+                    {
+                        description = xlRange.Cells[i, 8].Value2.ToString();
+                    }
+
+                    if (xlRange.Cells[i, 14] != null && xlRange.Cells[i, 14].Value2 != null)
+                    {
+                        Product = xlRange.Cells[i, 14].Value2.ToString();
+                    }
+                    if (xlRange.Cells[i, 15] != null && xlRange.Cells[i, 15].Value2 != null)
+                    {
+                        Component = xlRange.Cells[i, 15].Value2.ToString();
+                    }
+                    //if (xlRange.Cells[i, 16] != null && xlRange.Cells[i, 16].Value2 != null)
+                    //{
+                    //    AutomationStatus = xlRange.Cells[i, 16].Value2.ToString();
+                    //}
+                    if (xlRange.Cells[i, 17] != null && xlRange.Cells[i, 17].Value2 != null)
+                    {
+                        AutomationClassName = xlRange.Cells[i, 17].Value2.ToString();
+                    }
+                    if (xlRange.Cells[i, 18] != null && xlRange.Cells[i, 18].Value2 != null)
+                    {
+                        AutomationID = xlRange.Cells[i, 18].Value2.ToString();
+                    }
+                    if (xlRange.Cells[i, 19] != null && xlRange.Cells[i, 19].Value2 != null)
+                    {
+                        ReasonforNotAutomating = xlRange.Cells[i, 19].Value2.ToString();
+                    }
+                    if (xlRange.Cells[i, 20] != null && xlRange.Cells[i, 20].Value2 != null)
+                    {
+                        DeploymentMode = xlRange.Cells[i, 20].Value2.ToString();
+                    }
+                    if (xlRange.Cells[i, 21] != null && xlRange.Cells[i, 21].Value2 != null)
+                    {
+                        PAMTags = xlRange.Cells[i, 21].Value2.ToString();
+                    }
+                    if (xlRange.Cells[i, 22] != null && xlRange.Cells[i, 22].Value2 != null)
+                    {
+                        Applicable = xlRange.Cells[i, 22].Value2.ToString();
+                    }
+                    extraFields.Add(new Tuple<string, string>("Product", Product));
+                    extraFields.Add(new Tuple<string, string>("Component", Component));
+                    extraFields.Add(new Tuple<string, string>("AutomationClassName", AutomationClassName));
+                    extraFields.Add(new Tuple<string, string>("Automation ID", AutomationID));
+                    extraFields.Add(new Tuple<string, string>("Reason for Not Automating", ReasonforNotAutomating));
+                    extraFields.Add(new Tuple<string, string>("Deployment Mode", DeploymentMode));
+                    extraFields.Add(new Tuple<string, string>("PAM Tags", PAMTags));
+                    extraFields.Add(new Tuple<string, string>("Applicable", Applicable));
+
+                    if(xlRange.Cells[i, 5] != null && xlRange.Cells[i, 5].Value2 != null)
+                    {
+                        do
+                        {
+                            string attachmentUrl = ((Excel.Range)xlRange.Cells[i, 5]).Cells.Hyperlinks[1].Address;
+                            attachments.Add(AttachmentBaseDirectory + attachmentUrl);
+                            i++;
+                        } while ((xlRange.Cells[i, 3] == null || xlRange.Cells[i, 3].Value2 == null) && (xlRange.Cells[i, 5] != null && xlRange.Cells[i, 5].Value2 != null));
+                        i--;
+                    }
+
+                    VSTSOperations.CreateAreaPath(areaPathStack.Peek(), uri, project.Guid);
+                    VSTSOperations.CreateNewTestCase(uri, project, title, areaPathStack.Peek(), "RahulTCMigration", description, "Rahul Rao", steps, extraFields,attachments);
+
+                }               
+            }
+
+            //cleanup
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            Marshal.ReleaseComObject(xlRange);
+            Marshal.ReleaseComObject(xlWorksheet);
+
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+        }
+    }
+}
---
+++ VSTSTestCasesMigration/VSTSOperations.cs
@@ -0,0 +1,233 @@
+﻿using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.TestManagement.Client;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+using Microsoft.TeamFoundation.WorkItemTracking.Internals;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSTSTestCasesMigration
+{
+    public class VSTSOperations
+    {
+        public class ActionResult
+        {
+            public bool Success { get; set; }
+            public List<string> ErrorCodes { get; set; }
+            public int Id { get; set; }
+        }
+        private static ActionResult CheckValidationResult(WorkItem workItem)
+        {
+            var validationResult = workItem.Validate();
+
+            ActionResult result = null;
+            if (validationResult.Count == 0)
+            {
+                // Save the new work item.
+                workItem.Save();
+
+                result = new ActionResult()
+                {
+                    Success = true,
+                    Id = workItem.Id
+                };
+            }
+            else
+            {
+                //result = ParseValidation(validationResult);
+                throw new Exception("Invalid Fields");
+            }
+
+            return result;
+        }
+        private static WorkItem GetWorkItem(string uri, int testedWorkItemId)
+        {
+            TfsTeamProjectCollection tfs;
+
+            tfs = TfsTeamProjectCollectionFactory.GetTeamProjectCollection(new Uri(uri)); // https://mytfs.visualstudio.com/DefaultCollection
+            tfs.Authenticate();
+
+            var workItemStore = new WorkItemStore(tfs);
+            WorkItem workItem = workItemStore.GetWorkItem(testedWorkItemId);
+
+            return workItem;
+
+        }
+        public static Project GetTeamProject(string uri, string name)
+        {
+            TfsTeamProjectCollection tfs;
+
+            tfs = TfsTeamProjectCollectionFactory.GetTeamProjectCollection(new Uri(uri)); // https://mytfs.visualstudio.com/DefaultCollection
+            tfs.Authenticate();
+
+            var workItemStore = new WorkItemStore(tfs);
+
+            var project = (from Project pr in workItemStore.Projects
+                           where pr.Name == name
+                           select pr).FirstOrDefault();
+            if (project == null)
+                throw new Exception($"Unable to find {name} in {uri}");
+
+            return project;
+        }
+        public static void ManageTestPlans(string uri, Project project, string testPlanName)
+        {
+            try
+            {
+                TfsTeamProjectCollection tfs;
+
+                tfs = TfsTeamProjectCollectionFactory.GetTeamProjectCollection(new Uri(uri)); // https://mytfs.visualstudio.com/DefaultCollection
+                tfs.Authenticate();
+
+                ITestManagementService service = (ITestManagementService)tfs.GetService(typeof(ITestManagementService));
+                ITestManagementTeamProject testProject = service.GetTeamProject(project);
+
+                //Create TestPlan
+                ITestPlan newTestPlan = testProject.TestPlans.Create();
+                newTestPlan.Name = testPlanName;
+                newTestPlan.Owner = tfs.AuthorizedIdentity;
+                newTestPlan.Save();
+
+                //Get all are paths
+                NodeCollection nodeCollection = testProject.WitProject.AreaRootNodes;
+
+                foreach (Node node in nodeCollection)
+                {
+                    // Create Static Test Suite and set name based on area path
+                    IStaticTestSuite stsuite = testProject.TestSuites.CreateStatic();
+                    stsuite.Title = node.Name;
+
+                    newTestPlan.RootSuite.Entries.Add(stsuite);
+                    newTestPlan.Save();
+
+                    if (node.HasChildNodes)
+                    {
+                        CreateTestSuites(testProject, newTestPlan, node, stsuite, testProject.TeamProjectName + "\\" + node.Name);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+
+        private static void CreateTestSuites(ITestManagementTeamProject testProject, ITestPlan newTestPlan, Node node, IStaticTestSuite parentSuite, string parentAreaPath)
+        {
+            NodeCollection childNodes = node.ChildNodes;
+
+            foreach (Node childNode in childNodes)
+            {
+                IStaticTestSuite newsuite = testProject.TestSuites.CreateStatic();
+                newsuite.Title = childNode.Name;
+                parentSuite.Entries.Add(newsuite);
+                newTestPlan.Save();
+
+                //find tc's based on areapath
+                IEnumerable<ITestCase> tcs = testProject.TestCases.Query(string.Format("SELECT [System.Id], [System.Title] FROM WorkItems WHERE [System.TeamProject]='{0}' AND [System.AreaPath]='{1}' AND [System.WorkItemType]='Test Case'", testProject.TeamProjectName, parentAreaPath + "\\" + childNode.Name));
+                //Add the above entries to the static test suite.
+                foreach (ITestCase tc in tcs)
+                {
+                    newsuite.Entries.Add(tc);
+                }
+
+                newTestPlan.Save();
+
+                if (childNode.HasChildNodes)
+                {
+                    CreateTestSuites(testProject, newTestPlan, childNode, newsuite, parentAreaPath + "\\" + childNode.Name);
+                }
+            }
+        }
+
+        private static void AddTestCaseSteps(string uri, Project project, int testCaseId, Tuple<string, string>[] steps)
+        {
+            TfsTeamProjectCollection tfs;
+
+            tfs = TfsTeamProjectCollectionFactory.GetTeamProjectCollection(new Uri(uri)); // https://mytfs.visualstudio.com/DefaultCollection
+            tfs.Authenticate();
+
+            ITestManagementService service = (ITestManagementService)tfs.GetService(typeof(ITestManagementService));
+            ITestManagementTeamProject testProject = service.GetTeamProject(project);
+            ITestCase testCase = testProject.TestCases.Find(testCaseId);
+
+            foreach (var step in steps)
+            {
+                ITestStep newStep = testCase.CreateTestStep();
+                step.Item1.Replace(':', ' ');
+                newStep.Title = step.Item1.Replace(':', ' ');
+                newStep.ExpectedResult = step.Item2;
+
+                testCase.Actions.Add(newStep);
+            }
+            testCase.Save();
+        }
+
+        public static void CreateAreaPath(string areaPath, string uri, Guid projectGUID)
+        {
+            try
+            {
+                var node = new Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models.WorkItemClassificationNode();
+                node.StructureType = Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models.TreeNodeStructureType.Area;
+                string areaName = areaPath.Substring(areaPath.LastIndexOf('\\') + 1);
+                string pathToParentNode = areaPath.Substring(areaPath.IndexOf('\\') + 1, areaPath.LastIndexOf('\\') - areaPath.IndexOf('\\'));
+                node.Name = areaName;
+
+                string PAT = "izuresvf3e4tkxdgvwc5rgam4y5gvgc2lihsp5b3rw7yjyk2zxpq";
+                VssConnection connection = new VssConnection(new Uri(uri), new VssBasicCredential(string.Empty, PAT));
+                var client = connection.GetClient<WorkItemTrackingHttpClient>();
+
+                var result = client.CreateOrUpdateClassificationNodeAsync(node, projectGUID, Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models.TreeStructureGroup.Areas, pathToParentNode).Result;
+            }
+            catch (AggregateException ex)
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        public static ActionResult CreateNewTestCase(string uri, Project project, string title, string areaPath, string iterationPath, string description, string assignee, Tuple<string, string>[] reproductionSteps, List<Tuple<string, string>> extraFields, List<string> attachments, object tags = null)
+        {
+            WorkItemType workItemType = project.WorkItemTypes["Test Case"];
+
+            // Create the work item. 
+            WorkItem newTestCase = new WorkItem(workItemType);
+            newTestCase.Title = title;
+            newTestCase.Description = description;
+            newTestCase.AreaPath = areaPath;
+            newTestCase.IterationPath = iterationPath;
+            newTestCase.Fields["Assigned To"].Value = assignee;
+
+            foreach (var attachment in attachments)
+            {
+                newTestCase.Attachments.Add(new Attachment(attachment, "added by rahul rao"));
+            }
+
+            foreach (var extrafield in extraFields)
+            {
+                newTestCase.Fields[extrafield.Item1].Value = extrafield.Item2;
+            }
+
+            // Copy tags
+            if (tags != null)
+                newTestCase.Fields["Tags"].Value = tags;
+
+            ActionResult result = CheckValidationResult(newTestCase);
+            if (result.Success)
+            {
+                AddTestCaseSteps(uri, project, result.Id, reproductionSteps);
+            }
+
+            return result;
+        }
+    }
+}
